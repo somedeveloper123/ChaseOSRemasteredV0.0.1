533f3fa65b5e522bdad23d9337550d9f9b48b2e7
Chase Pham <chasep0842@outlook.com>
2021-08-04 19:21:57 -0400

    First

--- ChasesProject/Program.cs
+++ ChasesProject/Program.cs
@@ -8,10 +8,7 @@ namespace ChasesProject
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+
         static void Main()
         {
 
